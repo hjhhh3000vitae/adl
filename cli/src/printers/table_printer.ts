7428aa218260fdb9bfb3a87e34605af5dddc26a8
He Huang <heh@microsoft.com>
2020-04-16 01:14:09 -0700

    Finish up flushOutput

--- cli/src/printers/table_printer.ts
+++ cli/src/printers/table_printer.ts
@@ -2,15 +2,23 @@ import { printer } from './printer'
 
 import * as adlruntime from '@azure-tools/adl.runtime'
 
-class tableRow {
-    public model: string;
-    public version: string;
-    public type: string;
-    public property: string;
-    public dataType: string;
-    public constraints: string;
-
-
+class tableRow implements Iterable<[number, string, string]> {
+  constructor(
+    public model: string,
+    public version: string,
+    public type: string,
+    public property: string,
+    public dataType: string,
+    public constraints: string) {}
+    
+  *[Symbol.iterator](): Iterator<[number, string, string]> {
+    yield [0, "model", this.model];
+    yield [1, "version", this.version];
+    yield [2, "type", this.type];
+    yield [3, "property", this.property];
+    yield [4, "dataType", this.dataType];
+    yield [5, "constraints", this.constraints];
+  }
 }
 
 export class tablePrinter implements printer {
@@ -54,21 +62,48 @@ export class tablePrinter implements printer {
   }
 
   public flushOutput(): void {
-    const header: tableRow = {
-      model: "model",
-      version: "version",
-      type: "type",
-      property: "property",
-      dataType: "dataType",
-      constraints: "constraints"
-    };
+    const header = new tableRow("model", "version", "type", "property", "dataType", "constraints");
 
-    //let columnWidth: Array<number>;
-    //for (element of header) {
+    let columnWidth = new Array<number>();
+    for (const [index, key, value] of header) {
+      columnWidth.push(value.length);
+    }
 
-    //}
+    for (const row of this._output_cache) {
+      for (const [index, key, value] of row) {
+        columnWidth[index] = Math.max(columnWidth[index], value.length);
+      }
+    }
 
-    console.table(this._output_cache);
+    let outputLine = "";
+    for (const [index, key, value] of header) {
+      if (value.length == 0) {
+        outputLine += "null";
+        outputLine += " ".repeat(columnWidth[index]);
+      }
+      else {
+        outputLine += value;
+        outputLine += " ".repeat(columnWidth[index] + 4 - value.length);
+      }
+    }
+    console.log(outputLine);
+
+    for (const row of this._output_cache) {
+      let outputLine = "";
+      for (const [index, key, value] of row) {
+        if (value.length == 0) {
+          outputLine += "null";
+          outputLine += " ".repeat(columnWidth[index]);
+        }
+        else {
+          outputLine += value;
+          outputLine += " ".repeat(columnWidth[index] + 4 - value.length);
+        }
+      }
+      console.log(outputLine);
+    }
+
+    this._output_cache = new Array<tableRow>();
   }
 
   /*
@@ -79,14 +114,13 @@ export class tablePrinter implements printer {
     for (const prop of model.Properties) {
       if (prop.isRemoved) continue;
 
-      this._output_cache.push({
-        model: this._model, 
-        version: version, 
-        type: model.Name, 
-        property: propertyPrefix + prop.Name, 
-        dataType: `${prop.DataTypeName}/${prop.AliasDataTypeName}`, 
-        constraints: this.getPropertiesConstraintsAsText(prop)
-      });
+      this._output_cache.push(new tableRow(
+        /* model */ this._model, 
+        /* version */ version, 
+        /* type */ model.Name, 
+        /* property */ propertyPrefix + prop.Name, 
+        /* dataType */ `${prop.DataTypeName}/${prop.AliasDataTypeName}`, 
+        /* constraints */ this.getPropertiesConstraintsAsText(prop)));
 
       if(prop.DataTypeKind == adlruntime.PropertyDataTypeKind.Complex ||
         prop.DataTypeKind == adlruntime.PropertyDataTypeKind.ComplexArray ||
